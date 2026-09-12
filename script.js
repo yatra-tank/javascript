ef83b3ce9c61366521401206bd699776751bbd85
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-12 15:42:03 +0530

    js practice

---
+++ script.js
@@ -0,0 +1,66 @@
+// function division(p, q){
+//     console.log(p/q)
+// };
+// division(20,10);
+
+// function calculator(a, b){
+//     console.log(a + b);
+//     console.log(a - b);
+//     console.log(a * b);
+//     console.log(a / b);
+// };
+// calculator(20, 10);
+
+// function greet(){
+//     return "Hello Everyone"
+// };
+// console.log(greet());
+
+// var baseRent = 1000;
+// var maintainanceCharges = 500;
+// var monthlyRent = baseRent + maintainanceCharges;
+// console.log("The total monthly rent is:", monthlyRent, "Rs");
+
+// var wallet = 7000;
+// var spent = 179;
+// var remainingAmount = wallet - spent;
+// console.log("The remaining amount in the wallet is:", remainingAmount, "Rs")
+
+// var totalTime = 60;
+// var timeSpent = 24;
+// var timeLeft = totalTime - timeSpent;
+// console.log("The time remaining to complete a task is:", timeLeft, "minutes.")
+
+// var totalChocolates = 97;
+// var totalFriends = 4;
+// var chocolatesLeft = totalChocolates%totalFriends
+// console.log("The total amount of chocolates left, after distribution is:", chocolatesLeft)
+
+// var n = 1234;
+// var last2Digits = n%100;
+// console.log(last2Digits)
+
+// var totalMinutes = 135;
+// var remainingMinutes = totalMinutes % 60;
+// var hours = Math.floor(totalMinutes / 60);
+// console.log("Total Minutes:", totalMinutes);
+// console.log("Remaining time:", hours, ":",remainingMinutes);
+
+// var num = 3;
+// var pow = 2;
+// var exp = num ** pow;
+// console.log(exp)
+
+// var num = 27;
+// var root = num ** (1/3);
+// console.log(root);
+
+// var side = 5;
+// var area = side ** 2;
+// console.log("The area of the garden is:", area);
+
+// var maxLoginAttempts = 5;
+// console.log(maxLoginAttempts);
+
+// var num = 17%-4
+// console.log(num)
